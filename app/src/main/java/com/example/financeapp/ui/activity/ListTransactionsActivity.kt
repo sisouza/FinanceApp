ef5ea6229d4827a1f0845848cfbc1dcd34bd7379
Sidia <sidia.reis1@gmail.com>
2022-02-13 19:44:29 -0800

    chore:Add List Transaction Activity

---
+++ app/src/main/java/com/example/financeapp/ui/activity/ListTransactionsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.financeapp.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.financeapp.R
+
+class ListTransactionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_transactions)
+    }
+
+}

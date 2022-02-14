b09ccf5f08fe439ee5d9eb874e953d3ff4e238ac
Sidia <sidia.reis1@gmail.com>
2022-02-13 20:34:57 -0800

    feat:Init set transactions in Activity

--- app/src/main/java/com/example/financeapp/ui/activity/ListTransactionsActivity.kt
+++ app/src/main/java/com/example/financeapp/ui/activity/ListTransactionsActivity.kt
@@ -9,6 +9,13 @@ class ListTransactionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_transactions)
+
+        val transactions = listOf(
+            "Coffee - R$ 20,50",
+            "Cake - R$ 100,00"
+        )
+
+
     }
 
 }
